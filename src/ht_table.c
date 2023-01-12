3be62722948325a25e4f3e83aba94e39ee43e121
askertsos <aggelosskertsos@gmail.com>
2023-01-12 20:02:07 +0200

    Removed useless ht_open_files array

--- src/ht_table.c
+++ src/ht_table.c
@@ -11,7 +11,6 @@
 #define DB_ROOT "ht_databases/ht_"
 // Store number of created databases
 int open_ht_files_counter = 0;
-HT_info* open_ht_files[100];
 int max_records_per_block = BF_BLOCK_SIZE / sizeof(Record);
 
 
@@ -97,15 +96,11 @@ HT_info* HT_OpenFile(char *fileName){
 
   CALL_OR_DIE(BF_UnpinBlock(block));
 
-  for(int i = 0; i < BF_MAX_OPEN_FILES; i++){
-    if (open_ht_files[i] == NULL){
-      open_ht_files[i] = file_info;
-      open_ht_files[i]->position_in_open_files = i;
+  if (open_ht_files_counter < BF_MAX_OPEN_FILES){
       open_ht_files_counter++;
       BF_Block_Destroy(&block);
       log_info("Opened file %s with info : {fd = %d | buckets = %d | type = %d | position = %d}",fileName,file_info->fd,file_info->buckets,file_info->type, file_info->position_in_open_files);
       return file_info;
-    }
   }
 
   BF_Block_Destroy(&block);
@@ -114,14 +109,9 @@ HT_info* HT_OpenFile(char *fileName){
 
 int HT_CloseFile( HT_info* HT_info ){
   log_info("Closing file with info: {fd = %d | buckets = %d | type = %d | position = %d}",HT_info->fd,HT_info->buckets,HT_info->type,HT_info->position_in_open_files);
-  for(int i=0;i<BF_MAX_OPEN_FILES;i++){
-    if( open_ht_files[i] != NULL && open_ht_files[i]->fd == HT_info->fd ){
-      BF_CloseFile(HT_info->fd);
-      free(open_ht_files[i]);
-      open_ht_files[i] = NULL;
-      open_ht_files_counter--;
-      return 0;
-    }
+  if( BF_CloseFile(HT_info->fd) == 0 ){
+    open_ht_files_counter--;
+    return 0;
   }
   return -1;
 }
